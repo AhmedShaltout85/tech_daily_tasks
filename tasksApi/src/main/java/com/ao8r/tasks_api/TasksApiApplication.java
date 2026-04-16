231e44d31299980930744857a0a20ee5155ff220
AhmedShaltout85 <eng.abozaher@gmail.com>
2026-04-16 14:13:04 +0200

    Last-Update: 2026-04-16

--- tasksApi/src/main/java/com/ao8r/tasks_api/TasksApiApplication.java
+++ tasksApi/src/main/java/com/ao8r/tasks_api/TasksApiApplication.java
@@ -2,9 +2,16 @@ package com.ao8r.tasks_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApiApplication {
+public class TasksApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TasksApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TasksApiApplication.class, args);
